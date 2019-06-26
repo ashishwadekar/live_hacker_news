860c631c39decc379dc93c79138df8fcb7f459c7
Ashish Wadekar <ashishawadekar@gmail.com>
2019-06-26 08:53:24 +0200

    Push data changes realtime over channels

--- lib/live_hacker_news_web/live/hn_post_live.ex
+++ lib/live_hacker_news_web/live/hn_post_live.ex
@@ -3,6 +3,9 @@ defmodule LiveHackerNewsWeb.HnPostLive do
   alias LiveHackerNews.Post
 
   def mount(_session, socket) do
+    # Create a topic for others to join
+    Phoenix.PubSub.subscribe(LiveHackerNews.PubSub, "hacker_news")
+
     # Assign a random funny name
     usernames = ["Flowers_Hnads","Websites_Ice_cream","Breakfast_YouTube","YouTube_Ice_cream_cone","Ice_cream_cone_Fence","Video_games_System","Cone_Toolbox","Video_games_Printer","Ring_Drugs","Ice_cream_Solar", "Dislike_Poop","Light_saber_Clock","Book_Shoe","Towel_BBQ","Ice_cream_cone_Prints","Sink_Bird","Plus_Laptop","Breakfast_Video_games","Comics_Trees","Android_Breakfast", "Monster_Poop","Websites_Water","Allergies_Crab","Fusion_Crab","Toolbox_Trees","Ice_cream_cone_Horse","Nuclear_Laptop","Fusion_Trees","Fusion_Printer","Water_Toilet", "Soda_Monster","Settings_Breakfast","Sink_Comics","Monster_Trees","Hnads_Whale","Comics_Websites","Prints_Fence","Elevator_Clock","Mail_Monster","Ice_cream_cone_Fusion", "Ring_Robot","Dog_Breakfast","Male_Websites","Dog_Shoe","Ring_Male","Websites_Printer","Leash_Bird","Light_saber_Cat","Breakfast_Boat","Clock_Printer"]
     username = Enum.random(usernames)
@@ -35,6 +38,7 @@ defmodule LiveHackerNewsWeb.HnPostLive do
     Post.create_link(link)
     posts = fetch_posts()
 
+    Phoenix.PubSub.broadcast(LiveHackerNews.PubSub, "hacker_news", {__MODULE__, "add_post", %{posts: posts}})
     {:noreply, assign(socket, form_state: "none", posts: posts)}
   end
 
@@ -44,6 +48,7 @@ defmodule LiveHackerNewsWeb.HnPostLive do
     Post.update_link(post, %{upvotes: post.upvotes + 1})
     posts = fetch_posts()
 
+    Phoenix.PubSub.broadcast(LiveHackerNews.PubSub, "hacker_news", {__MODULE__, "upvote", %{posts: posts}})
     {:noreply, assign(socket, form_state: "none", posts: posts)}
   end
 
@@ -59,6 +64,11 @@ defmodule LiveHackerNewsWeb.HnPostLive do
     posts = Post.get_links_for_past()
 
     {:noreply, assign(socket, form_state: "none", posts: posts)}
+  end
+
+  # Handle Channel Sockets
+  def handle_info({LiveHackerNewsWeb.HnPostLive, action, payload}, socket) do
+    {:noreply, assign(socket, payload)}
   end
 
 
